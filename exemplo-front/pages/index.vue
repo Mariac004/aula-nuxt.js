40e142f8cd1cb735d27f10d751aee80dae430c87
Mariac004 <claudia.maria@aluno.ifsp.edu.br>
2022-05-02 16:48:19 -0300

    Criação exemplo Nuxt.js

---
+++ exemplo-front/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Bem vindo ao Nuxt.js</h1>
+ <ul>
+  <li>Elemeneto 1</li>
+  <li>Elemeneto 2</li>
+  <li>Elemeneto 3</li>
+  </ul>
+ </div>
+</template>
+
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
